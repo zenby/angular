40a72e80169aa76c9252f7473cac30e0a13c2016
zenby <zenbysel@gmail.com>
2018-07-05 10:50:34 +0300

    add routerLinkActive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styles: [`
+    .active {
+        background-color: red;
+    }
+    `]
 })
 export class HeaderComponent {
 }
