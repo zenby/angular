4b9c301d5b1e84b0ee30af7b5795f48705e54dab
zenby <zenbysel@gmail.com>
2018-07-05 17:22:19 +0300

    add basic TD form

--- src/app/shoping-list/shoping-edit/shoping-edit.component.ts
+++ src/app/shoping-list/shoping-edit/shoping-edit.component.ts
@@ -1,6 +1,7 @@
 import { ShoppingListService } from './../shoping-list.service';
-import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../../shared/ingredient.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shoping-edit',
@@ -8,19 +9,21 @@ import { Ingredient } from '../../shared/ingredient.model';
   styleUrls: ['./shoping-edit.component.css']
 })
 export class ShopingEditComponent implements OnInit {
-  @ViewChild('nameInput') nameInputRef: ElementRef;
-  @ViewChild('amountInput') amountInputRef: ElementRef;
+
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
   }
 
-  onAddIngredient() {
-    const ingName = this.nameInputRef.nativeElement.value;
-    const ingAmount = this.amountInputRef.nativeElement.value;
-    const newIngredient = new Ingredient(ingName, ingAmount);
+  onAddIngredient(form: NgForm) {
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
+
+  }
+
+  onSubmit() {
 
   }
 }
