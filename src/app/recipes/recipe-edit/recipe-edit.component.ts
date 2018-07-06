e9fbef7439bfb318ed37b2dd9a5b2babdc4398ae
zenby <zenbysel@gmail.com>
2018-07-06 13:25:50 +0300

    add new ingredient at click 'add' button

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -63,4 +63,13 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onAddIngredient() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl(),
+      })
+    );
+  }
+
 }
