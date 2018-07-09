99d8b81abd244bdc75d8b611931298f2073addf4
zenby <zenbysel@gmail.com>
2018-07-09 14:16:38 +0300

    add authGuard for editing pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MaterialModule } from './material/material.module';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
+import { AuthGuard } from './auth/auth-guard.service';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,7 @@ import { AuthService } from './auth/auth.service';
     AppRoutingModule,
     MaterialModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {
