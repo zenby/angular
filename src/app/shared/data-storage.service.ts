6abc585af03eb194d773657bc42c8ffc27e840c7
zenby <zenbysel@gmail.com>
2018-07-09 13:50:50 +0300

    add authenthentication permission to the user to fetch/save data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -4,21 +4,25 @@ import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
 import { map } from 'rxjs/operators';
+import { AuthService } from '../auth/auth.service';
 
 const URL = 'https://ng-project-c4248.firebaseio.com/data.json';
 
 @Injectable()
 export class DataStorageService {
   constructor(private http: HttpClient,
-              private recipeService: RecipeService) {
+              private recipeService: RecipeService,
+              private authService: AuthService) {
   }
 
   storeRecipes() {
-    return this.http.put(URL, this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+    return this.http.put(`${URL}?auth=${token}`, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get(URL)
+    const token = this.authService.getToken();
+    this.http.get(`${URL}?auth=${token}`)
       .pipe(
         map(
           (recipes: Recipe[]) => {
