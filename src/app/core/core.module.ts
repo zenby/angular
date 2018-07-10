e0a3ccb1c5b57ad3044c020343fa6e9916550d32
zenby <zenbysel@gmail.com>
2018-07-10 14:57:52 +0300

    add logging interceptor (in)

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -10,6 +10,7 @@ import { RecipeService } from '../recipes/recipe.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { ShoppingListService } from '../shoping-list/shoping-list.service';
 import { AuthInterceptor } from '../shared/auth.interceptor';
+import { LoggingInterceptor } from '../shared/logging.interceptor';
 
 @NgModule({
   declarations: [
@@ -29,7 +30,8 @@ import { AuthInterceptor } from '../shared/auth.interceptor';
     RecipeService,
     DataStorageService,
     AuthService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true},
   ]
 })
 export class CoreModule {
---
+++ src/app/shared/logging.interceptor.ts
@@ -0,0 +1,17 @@
+import { HttpHandler, HttpInterceptor, HttpRequest, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+export class LoggingInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(
+      tap(
+        event => {
+          console.log('Logging interceptor', event);
+        }
+      )
+    );
+  }
+}
+
+
