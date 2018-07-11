11c5c2959d2031bb9d7ab1e69f07341308bc9188
zenby <zenbysel@gmail.com>
2018-07-11 15:33:18 +0300

    add signin ngrx effects

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,9 +1,10 @@
 import { Actions, Effect } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
 import * as firebase from 'firebase';
-import { fromPromise } from 'rxjs/internal/observable/fromPromise';
+import { Router } from '@angular/router';
 
 import * as AuthActions from './auth.actions';
+import { fromPromise } from 'rxjs/internal/observable/fromPromise';
 import { mergeMap, map, switchMap } from 'rxjs/internal/operators';
 
 @Injectable()
@@ -22,6 +23,7 @@ export class AuthEffects {
         return fromPromise(firebase.auth().currentUser.getIdToken());
       }),
       mergeMap((token: string) => {
+        this.router.navigate(['/']);
         return [
           {
             type: AuthActions.SIGNUP
@@ -34,7 +36,35 @@ export class AuthEffects {
       })
     );
 
-  constructor(private actions$: Actions) {
+  @Effect()
+  authSignIn = this.actions$
+    .ofType(AuthActions.TRY_SIGNIN)
+    .pipe(
+      map((action: AuthActions.TrySignin) => {
+        return action.payload;
+      }),
+      switchMap((authData: { username: string, password: string }) => {
+        return fromPromise(firebase.auth().signInWithEmailAndPassword(authData.username, authData.password));
+      }),
+      switchMap(() => {
+        return fromPromise(firebase.auth().currentUser.getIdToken());
+      }),
+      mergeMap((token: string) => {
+        this.router.navigate(['/']);
+        return [
+          {
+            type: AuthActions.SIGNIN
+          },
+          {
+            type: AuthActions.SET_TOKEN,
+            payload: token
+          }
+        ];
+      })
+    );
+
+  constructor(private actions$: Actions,
+              private router: Router) {
   }
 
 
