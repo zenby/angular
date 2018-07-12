07295ed7caeebba01864c73d2d8ce5bc4d83a95b
zenby <zenbysel@gmail.com>
2018-07-12 12:27:31 +0300

    add animation when delete/add to shopping list

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -3,11 +3,32 @@ import { Subscription } from 'rxjs';
 
 import { ShoppingListService } from './shoping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
-  styleUrls: ['./shoping-list.component.css']
+  styleUrls: ['./shoping-list.component.css'],
+  animations: [
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({ // requires initial styles that will be implemented before animation
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+  ]
 })
 export class ShopingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
