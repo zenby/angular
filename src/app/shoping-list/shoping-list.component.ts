fb3a313dc00a35a66002598b26d7ff87bc7a6a53
zenby <zenbysel@gmail.com>
2018-07-12 12:37:57 +0300

    add keyframes to animation when delete/add to shopping list

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -3,14 +3,14 @@ import { Subscription } from 'rxjs';
 
 import { ShoppingListService } from './shoping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css'],
   animations: [
-    trigger('list1', [
+    trigger('list2', [
       state('in', style({
         opacity: 1,
         transform: 'translateX(0)'
@@ -21,6 +21,44 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
           transform: 'translateX(-100px)'
         }),
         animate(300)]),
+      transition('* => void', [
+        animate(300, style({
+          transform: 'translateX(100px)',
+          opacity: 0
+        }))
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        // divides all the time to equal parts (if no offset) to styles
+        animate(1000, keyframes([
+          style({
+            transform: 'translateX(-100px)',
+            opacity: 0,
+            // time of the style
+            offset: 0
+          }),
+          style({
+            transform: 'translateX(-50px)',
+            opacity: 0.5,
+            offset: 0.3
+          }),
+          style({
+            transform: 'translateX(-20px)',
+            opacity: 1,
+            offset: 0.8
+          }),
+          style({
+            transform: 'translateX(0px)',
+            opacity: 1,
+            offset: 1
+          }),
+        ]))
+      ]),
       transition('* => void', [
         animate(300, style({
           transform: 'translateX(100px)',
