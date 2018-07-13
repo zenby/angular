6672c60ee6a93d153d6792e0ef83bce597d23eda
zenby <zenbysel@gmail.com>
2018-07-13 16:42:53 +0300

    remove control buttons if user is not signed in

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import * as fromRecipe from '../store/recipe.reducers';
+import * as fromAuth from '../../auth/store/auth.reducers';
 
 @Component({
   selector: 'app-recipe-list',
@@ -25,16 +26,19 @@ import * as fromRecipe from '../store/recipe.reducers';
 })
 export class RecipeListComponent implements OnInit {
   recipeState: Observable<fromRecipe.State>;
+  authState: Observable<fromAuth.State>;
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private store: Store<fromRecipe.FeatureState>
+    private store: Store<fromRecipe.FeatureState>,
+    private authStore: Store<fromAuth.State>
   ) {
   }
 
   ngOnInit() {
     this.recipeState = this.store.select('recipes');
+    this.authState = this.authStore.select('auth');
   }
 
   onNewRecipe() {
