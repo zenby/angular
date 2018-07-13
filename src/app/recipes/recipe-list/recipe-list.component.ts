46be7ec9cdacefdcd78c1289442c2e71f8cf12f4
zenby <zenbysel@gmail.com>
2018-07-13 15:18:44 +0300

    add animation when delete item from recipe list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,13 +2,26 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 import * as fromRecipe from '../store/recipe.reducers';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
+  animations: [
+    trigger('recipeState', [
+      state('in', style({
+        opacity: 1,
+      })),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class RecipeListComponent implements OnInit {
   recipeState: Observable<fromRecipe.State>;
